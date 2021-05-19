cf33477e2134542563a0e66687da6ecba2b8a899
Carlos-juniorLUKE <70282446+Carlos-juniorLUKE@users.noreply.github.com>
2021-05-19 15:58:42 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:animacoes/screens/login/login_screen.dart';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animações',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
